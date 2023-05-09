f019787806cd1e7c36fd3cff507289e2a444ce5a
TetianaMamzer <105283969+TetianaMamzer@users.noreply.github.com>
2023-05-09 13:46:45 +0000

    Deploying to gh-pages from @ TetianaMamzer/Barber@fb455763c4a481346bf87322b133248bf4440148 🚀

---
+++ index.74b206ba.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-button]"),menu:document.querySelector("[data-menu]"),closeList:document.querySelector(".main-nav__list")};function n(){e.menu.classList.toggle("is-open")}console.log(e),e.openMenuBtn.addEventListener("click",n),e.closeList.addEventListener("click",n)}();
+//# sourceMappingURL=index.74b206ba.js.map
